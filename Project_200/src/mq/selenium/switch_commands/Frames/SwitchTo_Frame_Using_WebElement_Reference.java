a7afea284489793ce3144bc5935bdfa525a78f5c
sunilreddyg <sunilreddy.gajjala@gmail.com>
2022-04-12 10:44:22 +0530

    Add files via upload

---
+++ Project_200/src/mq/selenium/switch_commands/Frames/SwitchTo_Frame_Using_WebElement_Reference.java
@@ -0,0 +1,38 @@
+package switch_commands.Frames;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SwitchTo_Frame_Using_WebElement_Reference {
+
+	public static void main(String[] args) throws Exception {
+		//setting runtime environment variable
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\MINDQ\\Desktop\\recent_drivers\\geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();     //Launching browser method
+		driver.get("https://www.cleartrip.com/account/trips");
+	    driver.manage().window().maximize();
+	    Thread.sleep(5000);
+	    
+	    //switch to frame with WebElement Reference
+	    driver.switchTo().frame(driver.findElement(By.className("spinnerMedium")));
+	   
+	    WebElement Email=driver.findElement(By.xpath("//input[@id='email']"));
+	    Email.clear();
+	    Email.sendKeys("info.dsnr@gmail.com");
+	    
+	    
+	    WebElement tripId=driver.findElement(By.id("tripidSecond"));
+	    tripId.clear();
+	    tripId.sendKeys("67897897");
+	    
+	    //Get Controls back to mainpage
+	    driver.switchTo().defaultContent();
+	    
+	    WebElement Home=driver.findElement(By.xpath("//a[@title='Home']"));
+	    Home.click();
+
+	}
+
+}
